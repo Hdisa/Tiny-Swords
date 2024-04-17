e31b1dd47bf392e4d2ba759b3d714c8a14391405
Ruslan Matveev <ruslan.matveev03@mail.ru>
2024-04-18 00:40:39 +1000

    Repo Init

---
+++ Assets/script/Cellet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cellet : MonoBehaviour
+{
+    public GameObject cube;
+
+    private Camera _cam;
+
+    public LayerMask LayerMask;
+    
+    private void Awake()
+    {
+        _cam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit2D rayHit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+            Instantiate(cube, new Vector3(rayHit.point.x, rayHit.point.y), Quaternion.identity);
+        }
+        
+    }
+}

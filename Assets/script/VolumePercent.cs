fb33000428135602ba5fcd20208d48a20f1f0b16
tyanecaxa <kudashkina.2008@gmail.com>
2024-04-28 21:08:07 +0700

    PredEnd Menu 1
    Добавила циферки песент в настройки; чтобы нельзя было нажать другие кнопки, когда уже открыто окно. Переделала кнопки в окне истории и выходе. Всё, что могла сделать на данный момент - сделала, остальное - где-то под конец игры.

---
+++ Assets/script/VolumePercent.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TextMeshProUGUI))] 
+
+public class VolumePercent : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private string muteText = "0%";
+
+    private TextMeshProUGUI _text;
+
+    void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        ChangeValue(slider.value);
+        slider.onValueChanged.AddListener(ChangeValue);
+    }
+
+    private void ChangeValue(float v)
+    {
+        if (v < 0.001) _text.text = muteText;
+        else _text.text = v.ToString("F0") + "%";
+    }
+
+}

5e633243a0e0d539364728297b056daeafa239b4
tyanecaxa <kudashkina.2008@gmail.com>
2024-05-09 19:21:43 +0700

    Respawn resource
    Создала скрипт и префабы спавнеров.

--- Assets/script/SpawnResource.cs
+++ Assets/script/SpawnResource.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class SpawnResource : MonoBehaviour
 {
-    public List<GameObject> Resource;
-    public Transform respawnTransform;
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private float respawnTime = 5;
 
+    private float timer;
 
+    private void Start()
+    {
+        if(gameObject.TryGetComponent(out SpriteRenderer sr))
+             sr.enabled = false;
+        if (transform.childCount > 0) return;
+        Instantiate(prefab, transform);
+        timer = respawnTime;
+    }
+
+    private void Update()
+    {
+        if (transform.childCount > 0) return;
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            return;
+        }
+
+        Instantiate(prefab, transform);
+        timer = respawnTime;
+    }
 }
